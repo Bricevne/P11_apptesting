880fb787660808f663ad8cf8d471e7a20346b5b2
Brice Venayre <brice.venayre@gmail.com>
2022-12-07 18:44:46 +0100

    Update test_logout - session is cleared after logging out

--- gudlft_app/tests/unit_test/test_index.py
+++ gudlft_app/tests/unit_test/test_index.py
@@ -68,13 +68,17 @@ def test_index_no_email_address(client):
 
 
 def test_logout(app):
-    """Tests if the '/logout' url redirects to the index."""
+    """Tests if the '/logout' url redirects to the index and clear the email in the session."""
+
+    email = "test1@gmail.com"
     with app.test_client() as client:
+        with client.session_transaction() as session:
+            session['email'] = email
+            assert 'email' in session
+
         response = client.get("/logout", follow_redirects=True)
         assert response.status_code == 200
         # Check that the second request was to the index page.
         assert request.path == url_for('index')
-
-
-
-
+        with client.session_transaction() as session:
+            assert 'email' not in session
